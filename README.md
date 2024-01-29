b184708ba1031479413844b52cb680c30444f676
Paulo de Souza <paulo@desouza-tech.com>
2024-01-29 07:39:11 -0500

    Finished working port scanner with banner capturing

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # cybersecurity_scripts
 
-Penetration test scripts developed during classes and training.
+Penetration test scripts.
+
+
+If you need to perform port scanning, consider using NMAP instead of these scripts. 
+These were developed for educational purposed only. 
+
+https://nmap.org/
+
+NMAP is a wonderful tool with huge and granular capabilities for scanning networks in general, 
+and it is what I use when performing penetration tests in my network engineering adventures. 
 
 
--- port_scanner.py
+++ port_scanner.py
@@ -1,6 +1,16 @@
 import socket 
 from IPy import IP
 
+def scan(target, p1, p2=None):
+    converted_ip = check_ip(target)
+    print('\n' + '[-_0 Scanning Target] ' + str(target))
+    if p2 == None:
+        scan_port(converted_ip, p1)
+    else: 
+        for port in range(int(p1), int(p2+1)):
+            scan_port(converted_ip, port)
+    
+
 def check_ip(ip):
     try:
         # IP validation
@@ -11,6 +21,9 @@ def check_ip(ip):
         # if domain, get an IP
         return socket.gethostbyname(ip)
     
+def get_banner(s):
+    return s.recv(1024)
+
     
 def scan_port(ip, port):
     try:
@@ -18,35 +31,55 @@ def scan_port(ip, port):
         sock = socket.socket()
         sock.settimeout(0.2)
         sock.connect((ip, port))
-        print("PORT " + str(port) + " IS OPEN")
-        
+        try: 
+            banner = get_banner(sock)
+            print("PORT "+str(port) + " IS OPEN : " + str(banner.decode().strip('\n')))
+        except:
+            print("PORT " + str(port) + " IS OPEN")
+            
     except:
+        print("PORT " + str(port) + " IS CLOSED")        
         pass
         
     
     
-domain = input("Enter target(s) to scan: (separate targets with `n`)")
-#check if there are multiple targets
-ips = []
-TARGETS = []
-if "," in domain:
     
-    for target in domain.split(','): 
-        TARGETS.append(target.strip())
-        ip = check_ip(target.strip())
-        ips.append(ip)
-
-    for i, ip in enumerate(ips):  
-        print(TARGETS[i])
-        for i in range(1, 100):
-            scan_port(ip, i)
+    
+if __name__ == "__main__":    
+    
+    target = input("Enter target(s) to scan: (separate targets with `,`)")
+    
+    range_check = "check"
+    
+    while range_check not in "s" and range_check not in "r":
+        range_check = input("Do you want to scan a single port or a range of ports? (s / r)").lower()
+    
+    if range_check == "s":
+        port1 = int(input("Enter the port you want to be scanned."))
+    
+    else:
+        port1 = int(input("Enter the first port of the range you want to be scanned."))
+        port2 = int(input("Enter the last port of the range you want to be scanned."))
+        
+    
+    # check if there are multiple targets
+    if "," in target:
+    
+        if range_check == "s":    
+            for ip in target.split(','): 
+                scan(ip.strip(), port1)
+        
             
+        elif range_check == "r":    
+            for ip in target.split(','): 
+                scan(ip.strip(), port1, port2)        
+                
+    else:
+        
+        if range_check == "s":
+            scan(target, port1)
             
-else:
+        elif range_check == "r":
+            scan(target, port1, port2)
     
-    ip = check_ip(domain)
-
-    for i in range(1, 100):
-        scan_port(ip, i)
-    
-    
+        
